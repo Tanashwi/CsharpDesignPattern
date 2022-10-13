da80736d7d5880732dfd05ce469dc10b08b1a4b0
Tanmaya Thopate <tanmaya.thopate@ensono.com>
2022-10-13 16:54:01 +0530

    first commit

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,34 @@
+﻿namespace Singleton
+{
+  public class Logger
+  {
+    //This improves singleton behabior
+    private static readonly Lazy<Logger> _LazyLogger
+    = new Lazy<Logger>(()=> new Logger());
+    
+    //not needed now with Lazy<T>
+    //private static Logger? _instance;
+
+    public static Logger Instance
+    {
+        get
+        {
+            return _LazyLogger.Value;
+           // if(_instance == null)
+            //{
+                //_instance = new Logger();
+            //}
+            //return _instance;
+        }
+    }
+    protected Logger()
+    {
+
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"Message to Log: {message}");
+    }
+  }
+}
