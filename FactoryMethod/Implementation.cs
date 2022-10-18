a828d8199c0ec869f5d5265fecb6766f27b5a182
Tanmaya Thopate <tanmaya.thopate@ensono.com>
2022-10-18 11:36:26 +0530

    second commit

--- FactoryMethod/Implementation.cs
+++ FactoryMethod/Implementation.cs
@@ -1,4 +1,89 @@
 ﻿namespace FactoryMethod
 {
+    //open/closed principle
+    //single responsibility principle
    public abstract class DiscountService
+   {
+            public abstract int DiscountPercentage {get;}
+            public override string ToString() => GetType().Name;
+   }
+
+   ///Concrete implementation
+   public class CountryDiscountService: DiscountService
+   {
+        private readonly string _countryIdentifier;
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryIdentifier)
+                {
+                    case "US":
+                        return 10;
+                    default:
+                        return 20;
+                }
+            }
+        }
+}
+
+    public class CodeDiscountService: DiscountService
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage
+        {
+            get => 15;
+        }
+    }
+
+//Factory implementation = I could have created separate file, but keeping in same file for learning purpose
+
+    //Creator
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();        
+    }
+
+    //Concreate implementation
+
+    public class CountryDiscountFactory: DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    public class CodeDiscountFactory: DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
 }
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+
+var factories = new List<DiscountFactory> {
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("US")
+};
+
+foreach (var factory in factories)
+{
+    var discount = factory.CreateDiscountService();
+    Console.WriteLine($"Percentage {discount.DiscountPercentage} " + $"coming from {discount}");
+}
